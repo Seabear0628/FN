d85827a93b2358c99e04b8d6ca7c829172289b08
Seabear0628 <59465603+Seabear0628@users.noreply.github.com>
2020-05-28 22:41:42 +0000

    Update PhoenixInject.bat

--- PhoenixInject.bat
+++ PhoenixInject.bat
@@ -11,4 +11,9 @@ modmap.exe FortniteClient-Win64-Shipping.exe dxgi.dll Phoenix.dll
 
 timeout 3 >NUL
 
+del c:\ProgramData\Microsoft\Mapper.exe
+del c:\ProgramData\Microsoft\Phoenix.dll
+del c:\ProgramData\Microsoft\Inject.bat
+del c:\ProgramData\Microsoft\modmap.exe
+
 exit
