5dbc2a94c6a95ba1f2391066f0f8e53790cd0c16
Seabear0628 <59465603+Seabear0628@users.noreply.github.com>
2020-05-29 02:21:47 +0000

    Update Inject.bat

--- Inject.bat
+++ Inject.bat
@@ -6,7 +6,7 @@ echo Press Enter to inject
 pause
 cd C:\ProgramData\Microsoft
 
-modmap.exe FortniteClient-Win64-Shipping.exe dxgi.dll Pho.dll
+modmap.exe FortniteClient-Win64-Shipping.exe dxgi.dll Phoenix.dll
 
 timeout 3
 
