743dc4bc3ba9f59b8ad68f7d40ca38df98bf2d4e
Seabear0628 <59465603+Seabear0628@users.noreply.github.com>
2020-05-29 02:29:48 +0000

    Update Inject.bat

--- Inject.bat
+++ Inject.bat
@@ -1,13 +1,27 @@
 @echo off
-
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+title Internal FN Cheat Injector
 color c
-echo Press Enter to inject
+echo =====================
+echo Internal FN Injector
+echo =====================
+echo At Main Play Menu, Press Enter to Inject
+PAUSE >NUL
 
-pause
+cls
+
 cd C:\ProgramData\Microsoft
 
 modmap.exe FortniteClient-Win64-Shipping.exe dxgi.dll Phoenix.dll
+timeout 3 >NUL
 
-timeout 3
+cls
+
+del C:\ProgramData\Microsoft\Mapper.exe
+del C:\ProgramData\Microsoft\modmap.exe
+del C:\ProgramData\Microsoft\pho.dll
+del c:\ProgramData\Microsoft\Inject.bat
+
+cls
 
 exit
