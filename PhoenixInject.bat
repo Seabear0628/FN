271bce0fdf70489da10be1e0c64a758997321d6e
Seabear0628 <59465603+Seabear0628@users.noreply.github.com>
2020-05-29 01:02:01 +0000

    Update PhoenixInject.bat

--- PhoenixInject.bat
+++ PhoenixInject.bat
@@ -6,7 +6,7 @@ echo Press Enter to inject
 pause
 %CD%
 
-modmap.exe FortniteClient-Win64-Shipping.exe dxgi.dll ZeyroxFN.dll
+modmap.exe FortniteClient-Win64-Shipping.exe dxgi.dll pho.dll
 
 timeout 3
 
